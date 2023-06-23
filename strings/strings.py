1967438ba88e0360291cebc582d858aa63fd941f
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-23 16:50:57 -0300

    Add files via upload

---
+++ strings/strings.py
@@ -0,0 +1,30 @@
+"""
+Programa: strings
+Descrição: Este programa apresenta a estrutura de dados "strings" em Python para fins de aprendizado.
+Autor: F
+Data : 23/06/2023
+Versão: 0.0.1
+"""
+
+#Atribuição de variáveis
+
+primeira_string = "Casa"
+
+
+#Entrada de dados
+
+
+
+
+#Processamento de dados
+
+maiusculas_primeira_string = primeira_string.upper()
+minusculas_primeira_string = primeira_string.lower()
+
+
+
+#Saida de dadaos
+
+print(primeira_string)
+print(maiusculas_primeira_string)
+print(minusculas_primeira_string)
